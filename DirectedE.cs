1ad5fbe35a009be6487cc66ca2665cfd76191d9c
雅治 沼下 <190183@jc-21.jp>
2022-02-08 12:02:40 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectedE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kuriage
+{
+    public class kuriage
+    {
+
+        public static int kuriage10(int i)
+        {
+            if (i >= 5) i = 10;
+            if (i < 5) i = 0;
+            return i;
+        }
+
+
+    }
+}
---
+++ JetCoaster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kuriage
+{
+    public class DirectedE
+    {
+
+        public static int SEARCH
+            (int[] edgeFirst,int[] edgeNext,int start)
+        {
+            int N = edgeFirst.Length;
+            int[] current;
+            for (int i = 1; i < N; i++)
+            {
+
+                current[i] = edgeFirst[i];
+
+            }
+
+
+
+
+            return n;
+        }
+
+    }
+}
