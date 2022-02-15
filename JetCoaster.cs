d3bd1c1ca3da2f3425c79d87a8a3e1127c23a739
雅治 沼下 <190183@jc-21.jp>
2022-02-15 12:40:13 +0900

    JetCoaster完成

--- JetCoaster.cs
+++ JetCoaster.cs
@@ -6,24 +6,39 @@ using System.Threading.Tasks;
 
 namespace kuriage
 {
-    public class DirectedE
+    public class JetCoaster
     {
 
-        public static int SEARCH
-            (int[] edgeFirst,int[] edgeNext,int start)
+        public static int ride
+            (int k,int[] g,int r)
         {
-            int N = edgeFirst.Length;
-            int[] current;
-            for (int i = 1; i < N; i++)
-            {
 
-                current[i] = edgeFirst[i];
+            int[] copy = new int[g.Length];
+            Array.Copy(g, copy, g.Length);
 
+            //k定員
+            //g乗ろうとしてるグループ
+            //r動かす回数
+            int n = 0;//合計人数
+            for (int a = 0; a < r; a++){
+                int h = 0;//乗っている人数
+                for (int b = 0; b<g.Length; b++)
+                {
+                    if (h + g[0] > k)
+                    {
+                        break;
+                    }
+                    h += g[0];
+                    int t = g[0];
+                    for(int c = 0; c+1 < g.Length; c++)
+                    {
+                        g[c] = g[c + 1];
+                    }
+                    g[g.Length - 1] = t;
+                }
+                n += h;
             }
-
-
-
-
+            Array.Copy(copy,g,copy.Length);
             return n;
         }
 
