660873831c95259751a45948333dc2a000fc1190
雅治 沼下 <190183@jc-21.jp>
2022-02-18 16:17:16 +0900

    メソッド名を Kuriage10 →　kuriage10に修正

--- kuriage.cs
+++ kuriage.cs
@@ -9,7 +9,7 @@ namespace kuriage
     public class kuriage
     {
 
-        public static int Kuriage10(int i)
+        public static int kuriage10(int i)
         {
             if (i == 0) return 0;
             int a = 0;
